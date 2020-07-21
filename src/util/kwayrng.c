d4e6853b5cf1f3227294c3fff3e7a6e8398318dd
Paulo Fonseca <paguso@cin.ufpe.br>
2020-07-20 23:09:24 -0300

    kway indep rng bug fixes

--- src/util/kwayrng.c
+++ src/util/kwayrng.c
@@ -24,16 +24,44 @@ kwayrng *kwayrng_new(size_t k, size_t nbits)
     ERROR_ASSERT(nbits<64, "Maximum number of random bits is 63");
     kwayrng *ret = NEW(kwayrng);
     ret->k = k;
-    ret->maxval = prime_succ(1 << ( nbits - 1 ));
-    ret->coefs = NEW_ARR(uint64_t, k+1);
-    for (size_t i=0; i<=k; i++) {
+    ret->maxval = prime_succ((1 << nbits) - 1 );
+    ret->coefs = NEW_ARR(uint64_t, k);
+    for (size_t i=0; i<k; i++) {
         ret->coefs[i] = rand_range_uint64_t(1, ret->maxval);
     }
     ret->count = 0;
     return ret;
 }
 
-//kwayrng *kwayrng_new_with_seeds(size_t k, size_t nbits, uint64_t *seeds);
+
+kwayrng *kwayrng_new_with_coefs(size_t k, uint64_t *coefs, size_t nbits)
+{
+    ERROR_ASSERT(nbits<64, "Maximum number of random bits is 63");
+    kwayrng *ret = NEW(kwayrng);
+    ret->k = k;
+    ret->maxval = prime_succ((1 << nbits) - 1 );
+    ret->coefs = NEW_ARR(uint64_t, k);
+    for (size_t i=0; i<k; i++) {
+        ret->coefs[i] = coefs[i] % ret->maxval;
+    }
+    ret->count = 0;
+    return ret;
+}
+
+
+
+
+size_t kwayrng_k(kwayrng *rng)
+{
+    return rng->k;
+}
+
+
+const uint64_t *kwayrng_coefs(kwayrng *rng)
+{
+    return rng->coefs;
+}
+
 
 void kwayrng_reset(kwayrng *rng) {
     rng->count = 0;
@@ -47,7 +75,7 @@ uint64_t kwayrng_maxval(kwayrng *rng) {
 
 uint64_t kwayrng_next(kwayrng *rng) {
     uint64_t ret = 0;
-    for (uint64_t i=0, pow=1; i <= rng->k; i++) {
+    for (uint64_t i=0, pow=1; i < rng->k; i++) {
         ret = mod_sum(ret, mod_mult(rng->coefs[i], pow, rng->maxval), rng->maxval);
         pow = mod_mult(pow, rng->count, rng->maxval);
     }
--- test/kwayrngtest.c
+++ test/kwayrngtest.c
@@ -3,6 +3,7 @@
 
 #include "CuTest.h"
 
+#include "arrutil.h"
 #include "errlog.h"
 #include "kwayrng.h"
 
@@ -12,6 +13,11 @@ void test_kwayrng(CuTest *tc)
     size_t nbits = 4;
     for (size_t k=0; k<10; k++) {
         kwayrng *rng = kwayrng_new(k, nbits);
+        uint64_t *coef = kwayrng_coefs(rng);
+        //PRINT_ARR(coef, "coef", "%"PRIu64, 0, k, 10);       
+        for (size_t i=0; i<k; i++) {
+            DEBUG("coef[%zu] = %"PRIu64"\n", i, coef[i]);
+        }
         DEBUG("%zu-way independent sequence:\n", k);
         for (size_t i=0; i<kwayrng_maxval(rng); i++) {
             DEBUG("X[%zu] = %"PRIu64"\n", i,  kwayrng_next(rng));
